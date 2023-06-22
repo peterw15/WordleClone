cbbb6fcaec9522a41fdc65c5d26f5401645616e7
pegw2 <peterw1502@gmail.com>
2023-06-22 23:29:34 +0100

    updates

--- Scripts.js
+++ Scripts.js
@@ -10,8 +10,24 @@ function loadPage() {
     currentGuess = "";
     stillPlaying = true;
     lives = 5;
-    document.getElementById("numOfGuess").innerHTML = "Lives " + lives;
+    document.getElementById("numOfGuess").innerHTML = "Lives: " + lives;
     document.getElementById("entryBox").style.border = "black solid 5px";
+    // for (let i = 0; i < 5; i++) { 
+    //     guesses[i] = ""; 
+    //     document.getElementById("guess" + lives.toString()).innerHTML = "";
+    // }
+
+    document.getElementById("guess1").innerHTML = "";
+    document.getElementById("guess2").innerHTML = "";
+    document.getElementById("guess3").innerHTML = "";
+    document.getElementById("guess4").innerHTML = "";
+    document.getElementById("guess5").innerHTML = "";
+
+
+    document.getElementById("headerContainer").innerHTML = "Guess The Five Letter Word!";
+    document.getElementById("headerContainer").style.color = "white";
+    document.getElementById("Keyboard").style.backgroundColor = ""; 
+
     loadWord();
 
 }
@@ -52,14 +68,29 @@ function submitGuess() {
     if ((currentGuess.length == 5) && stillPlaying) {
         guesses[lives] = currentGuess;
         
-        document.getElementById("guess" + lives.toString()).innerHTML =  currentGuess;
+        let currGuess = document.getElementById("guess" + lives.toString());
+        currGuess.innerHTML = currentGuess.toUpperCase();
+        let newStr = "";
+        for(let i = 0; i < currGuess.innerHTML.length; i++) {
+            //alert(currGuess.innerHTML.charAt(i) + currentWord.charAt(i));
+            if (currGuess.innerHTML.charAt(i) == currentWord.charAt(i)) {
+                newStr += '<span style="color: lightGreen">' + currGuess.innerHTML.charAt(i) + '</span>';
+            }
+            else {
+                newStr += currGuess.innerHTML.charAt(i);
+            }
+        } 
+        currGuess.innerHTML = newStr
+
         lives--;
         document.getElementById("numOfGuess").innerHTML = "Lives: " + lives;
         if (currentGuess == currentWord) {
             document.getElementById("entryBox").style.border = "solid lightgreen 5px";
             stillPlaying = false;
+            result(true);
         }
         else {
+            checkLetters();
             document.getElementById("entryBox").style.border = "solid red 5px";
         }
         checkLives();
@@ -68,17 +99,30 @@ function submitGuess() {
     
 }
 
+function checkLetters() {
+
+}
+
 function checkLives() {
     if (lives == 0) {
         stillPlaying = false;
+        result(false)
     }
 }
 
 function result(res) {
     if (res) {
-
+        let txt = document.getElementById("headerContainer");
+        let kbd = document.getElementById("Keyboard");
+        txt.style.color = "lightGreen"; 
+        txt.innerHTML = "You Won!";
+        kbd.style.backgroundColor = "lightGreen";
     }
     else {
-        
+        let txt = document.getElementById("headerContainer");
+        let kbd = document.getElementById("Keyboard");
+        txt.style.color = "red";
+        txt.innerHTML = "You Lost!";
+        kbd.style.backgroundColor = 'rgb(158, 61, 61)';
     }
 }
